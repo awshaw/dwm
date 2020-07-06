43b503096dc684ea1a998370828c90305359a53e
awshaw <anthony.shaw212@gmail.com>
2020-07-06 01:11:53 +0000

    menu reposition patch and fix for center window name patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 6;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "noto-fonts-sans-display:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 // background color
